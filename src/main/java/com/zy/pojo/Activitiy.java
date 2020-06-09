4bfd9d227522eec16c62a09fe8fca28aeb564580
t184duyueqing <3051832751@qq.com>
2020-06-09 21:00:10 +0800

    low的一匹的活动2020年6月9日20:58:55(实体类补充)

---
+++ src/main/java/com/zy/pojo/Activitiy.java
@@ -0,0 +1,42 @@
+package com.zy.pojo;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Data
+public class Activitiy {
+
+	private Integer id;
+
+	/**
+	 * 活动名称
+	 */
+	private String activities;
+
+	/**
+	 * 赠送礼品
+	 */
+	private String gifts;
+
+	/**
+	 * 礼品数量
+	 */
+	private Integer num;
+
+	/**
+	 * 活动时间
+	 */
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+	private LocalDateTime datetime;
+
+	/**
+	 * 活动内容
+	 */
+	private String content;
+
+
+}
